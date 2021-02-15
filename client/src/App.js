7337b165fb63ef25e17c900b5b772549d990ae5e
oliver <olivercastroteran@gmail.com>
2021-02-14 23:54:31 -0400

    global state setup with redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import Form from './components/Form/Form';
 import Posts from './components/Posts/Posts';
 import memories from './images/memories.png';
+import { getPosts } from './store/actions/postsActions';
 import useStyles from './styles';
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './store/reducers/rootReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
